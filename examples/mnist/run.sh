198dada129f2143b6f626a50b82d45575f4c1115
Albert <huangsy1314@163.com>
2021-12-20 20:40:15 +0800

    update

--- examples/mnist/run.sh
+++ examples/mnist/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python test_trainer.py --nodes 4 --share_file /your_path/temp_share_file --data /your_path/mnist/data
+python train_dist.py --nodes 4 --share_file /your_path/temp_share_file --data /your_path/mnist/data
